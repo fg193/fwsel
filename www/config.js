a81cf99338f110f19cb008c3168435477d25517d
Arnie97 <arnie97@gmail.com>
2026-02-24 01:27:41 +0800

    Fit r2s.site

--- www/config.js
+++ www/config.js
@@ -5,10 +5,10 @@ var config = {
   show_help: true,
 
   // Versions list (optional if provided by .versions.json)
-  versions: ["24.10.5", "23.05.7", "21.02.7"],
+  versions: ["24.10.5", "24.10.4", "23.05.7", "21.02.7"],
 
   // Pre-selected version (optional if provided by .versions.json)
-  default_version: "23.05.4",
+  default_version: "24.10.4",
 
   // Image download URL (e.g. "https://downloads.openwrt.org")
   image_url: "https://mirrors.pku.edu.cn/immortalwrt",
